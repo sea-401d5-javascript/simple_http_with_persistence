9df2f78822bc436822237a35718ad839df4e892a
Dan Stineback <dstineback@gmail.com>
2016-05-19 11:22:49 -0700

    test working

--- test/test.js
+++ test/test.js
@@ -4,33 +4,33 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const expect = chai.expect;
 const request = chai.request;
-// const startServer = require(__dirname + '/../lib/server');
-var server;
+const startServer = require(__dirname + '/../lib/server');
+
 
 describe('the http server', () => {
 
-  it('should accept get requests to /notes and respond with list of notes', (done) => {
+  it('should test Get request', (done) => {
     request('localhost:3000')
     .get('/notes')
     .end((err, res) => {
       expect(err).to.eql(null);
       expect(res).to.have.status(200);
-      var files = fs.readdirSync(__dirname + '/../notes_test').toString();
+      var files = fs.readdirSync(__dirname + '/../notes').toString();
       expect(res.text).to.eql(files);
       done();
     });
   });
 
-  it('should accept json post requests to /notes and save the body in a file', (done) => {
-    var nextFile = fs.readdirSync(dir).length + 1;
+  it('should test post request', (done) => {
+    var nextFile = fs.readdirSync(__dirname + '/../notes').length + 1;
     request('localhost:3000')
     .post('/notes')
     .send({ name: 'test name' })
     .end((err, res) => {
       expect(err).to.eql(null);
       expect(res).to.have.status(200);
-      expect(res.text).to.eql('/notes');
-      fs.readFile(__dirname + '/../notes_test/' + nextFile + '.json', (err, data) => {
+      expect(res.text).to.eql('saved file ' + nextFile + '.json' + '\n');
+      fs.readFile(__dirname + '/../notes' + nextFile + '.json', (err, data) => {
         if (err) throw err;
         var parsed = JSON.parse(data);
         expect(parsed).to.eql({ name: 'test name' });
