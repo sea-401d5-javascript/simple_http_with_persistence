1df118b20b14706663a184d72642220c62e4af20
Ruben Sarkisyan <rubens24@uw.edu>
2016-05-19 14:41:14 -0700

    added tests and improved server file

--- ruben/server.js
+++ ruben/server.js
@@ -5,73 +5,28 @@ const stream = require('stream');
 const jsonParse = require('./promise');
 
 http.createServer((req, res) => {
-  //TODO res error
-  if (res === 'error')
-  //TODO req error
-
-  //TODO GET
-  if (req.url === '/' && req.method === "GET") {
-    //read File
-
-    //TODO file not found
-
-    let file = fs.createReadStream(__dirname + '/data/data.json');
-    file.pipe(res);
-
-    //send data back
-
-  } else if (req.url === "/" && req.method === 'POST') {
-
-    let bufArr = [];
-    let bufStr = '';
-    req.on('data', (data) => {
-      bufArr.push(data);
-      bufStr += data.toString();
-    });
-
-    req.on('end', () => {
-      //start processing
-
-      //validation
-      let str = bufArr.toString(); //array of buffers to string
-      let jsonObj = {};
-      // try {
-      //   jsonObj = JSON.parse(str);
-      // } catch (e) {
-      //   //tell client we have error
-      //   res.statusCode = 400;
-      //   res.write("Use json");
-      //   //log error
-      //   console.log("error validating");
-      // }
-
-      jsonParse(str).then((data) =>{  //data is a json obj
-        let validString = JSON.stringify(data);
-        //TODO error handling file write
-        let file = fs.createWriteStream(__dirname + '/data/data.json');
-        let bufferStream = new stream.PassThrough();
-        let inBuf = new Buffer(validString);
-        bufferStream.end(inBuf);
-        bufferStream.pipe(file);
-
-        res.statusCode = 200;
-        res.end("data written to file\n");
-
-      }, (err) =>{
-        res.statusCode = 400;
-        res.end("err");
-
-        console.log(err);
+  if (req.method === 'GET' && req.url === '/note') {
+    fs.readdir(__dirname + '/data', (err, data) => {
+      if (err) return res.end('err');
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
       });
-
+        res.write(JSON.stringify(data));
+        return res.end();
     });
-
-  } else {
-    //TODO deal with not found
-    res.status = 404;
-    res.end("File not found\n");
-
   }
+
+  if (req.method === 'POST' && req.url ==='/note') {
+    fs.readdir(__dirname + '/data', (err, data) => {
+      if (err) return res.end('err');
+      let fileName = data.length + '.json';
+      req.on('data', (data) => {
+      fs.writeFile(__dirname + '/data/' + fileName, data, (err) => {
+        res.end();
+      });
+    });
+  });
+}
 
 }).listen(3000, () => {
   console.log("server up at 3000");
