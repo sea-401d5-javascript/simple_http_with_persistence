533e1c5ff81f435a78bac761ee1e75b589addc17
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-19 12:03:49 -0700

    submitting

--- test/server_test.js
+++ test/server_test.js
@@ -5,10 +5,17 @@ const chaiHTTP = require('chai-http');
 const expect = chai.expect;
 chai.use(chaiHTTP);
 const request = chai.request;
+const fs = require('fs');
+var fileArr = [];
+var newFileArr = [];
 
 require('../server');
 
 describe('persistance tests', () => {
+  before('read files in notes', () => {
+    fileArr = fs.readdirSync(__dirname + '/../data');
+    console.log(fileArr);
+  });
   it('should do a GET request', (done) => {
     request('localhost:3000')
     .get('/notes')
@@ -22,13 +29,13 @@ describe('persistance tests', () => {
   });
   it('should do a POST request', (done) => {
     request('localhost:3000')
-      .get('/notes')
+      .post('/notes')
+      .send('{"test": "test"}')
       .end((err, res) => {
-        expect((err, res) => {
-          expect(err).to.eql(null);
-          expect(res).to.have.status(200);
-        })
-      })
-    })
-  })
+        newFileArr = fs.readdirSync(__dirname + '/../data');
+        expect(err).to.eql(null);
+        expect(newFileArr).to.not.eql(fileArr);
+        done();
+      });
+  });
 });
