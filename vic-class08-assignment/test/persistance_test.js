059c603cb78e33811686d54fed79ad53fec4ee38
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-05-19 12:52:53 -0700

    created server that responds to get-post requests, get request to notes lists saved files

---
+++ vic-class08-assignment/test/persistance_test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHTTP);
+const request = chai.request;
+
+require('../server');
+
+describe('HTTP test', () => {
+  it('should ')
+})
