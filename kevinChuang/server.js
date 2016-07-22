89ec15a58f886d71464ad38f19cf3856265035e8
Kevin Chuang <kcxinkun@gmail.com>
2016-07-22 11:39:46 -0700

    refactored server file, removed promise.js

--- kevinChuang/server.js
+++ kevinChuang/server.js
@@ -4,7 +4,6 @@
 const http = require('http');
 const fs = require('fs');
 const stream = require('stream');
-const jsonParse = require('./promise');
 
 exports = module.exports = {};
 
@@ -17,51 +16,31 @@ http.createServer((req,res)=> {
 
   }
 
-  if(req.url === '/' && req.method === 'POST'){
-    var bufferArray = [];
-    // var bufferString;
+  else if(req.url === '/' && req.method === 'POST'){
+    var bufferString = '';
     req.on('data',(data)=> {
-      bufferArray.push(data);
-      console.log('data',data);
-      // bufferString += data.toString();
-      console.log(bufferArray);
-      return res.end();
+      bufferString += data.toString();
     });
 
     req.on('end',()=> {
-      var string = bufferArray.toString();
-      console.log('string',string,typeof string);
-      // var jsonObject = {};
+      var newFile = (fs.readdirSync(__dirname + '/data/')).length + 1;
+      var file = fs.createWriteStream(__dirname + '/data/test' + newFile + '.json');
+      var bufferStream = new stream.PassThrough();
+      var incomingBuffer = new Buffer(bufferString);
 
-      jsonParse(string).then((data)=> {
-        console.log('data',data, typeof data);
-        var fileNum = fs.readdirSync(__dirname+'/data').length+1;
-        var file = fs.createWriteStream(__dirname + '/data/'+ fileNum +'.json');
-
-        console.log('fileNumber',fileNum);
-
-        var bufferStream = new stream.PassThrough();
-        var incomingBuffer = new Buffer(JSON.stringify(data));
-
-        console.log('incomingBuffer',incomingBuffer, typeof incomingBuffer);
-
-        bufferStream.end(incomingBuffer);
-        bufferStream.pipe(file);
-        res.statusCode = 200;
-        res.end(console.log("Data written to file"));
-      }, (err)=> {
-        res.statusCode = 400;
-        res.end(err);
-      });
+      bufferStream.end(incomingBuffer);
+      bufferStream.pipe(file);
+      res.statusCode=200;
+      return res.end('File written\n');
     });
-    return;
+  } else {
+    res.writeHead(404,{
+      'Content-Type' : 'text/html'
+    });
+    res.write('Not Found');
+    res.statusHead = 404;
   }
 
-  res.writeHead(404,{
-    'Content-Type' : 'text/html'
-  });
-  res.write('Not Found');
-  res.statusHead = 404;
 }).listen(3000,()=>{
   console.log('Listening on port 3000');
 });
